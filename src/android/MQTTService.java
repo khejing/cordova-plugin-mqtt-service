d8650c32677093009b3aef901326f2a645c4ff79
khejing <khejing@hotmail.com>
2015-08-20 00:52:27 +0800

    error method name

--- src/android/MQTTService.java
+++ src/android/MQTTService.java
@@ -3,6 +3,8 @@ package com.yang.eto1.CordovaPlugin;
 import android.util.Log;
 
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
@@ -10,7 +12,7 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttSecurityException;
 
 public class MQTTService extends CordovaPlugin{
-	public boolean test(){
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext){
 		Log.i("MQTTService", "test has been called");
 		return true;
 	}
